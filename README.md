97012ea476d48a1b9e9504971949b57575a03354
PierreBezuchow <axel@starosta.pl>
2018-05-18 17:57:31 +0200

    Validation and alerts

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 UI built with Skeleton
 
 ![alt-text](https://github.com/PierreBezuchow/book-list/blob/master/img/book-list-screenshot.png)
+
+### Functions:
+Validation:
+- input fields must not be empty
+
+Alerts:
+- success and error alerts disappearing after specific time
--- app.js
+++ app.js
@@ -24,6 +24,27 @@ UI.prototype.addBookToList = function(book){
   list.appendChild(row);
 }
 
+// Show alert
+UI.prototype.showAlert = function(message, className) {
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 // Clear fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -45,11 +66,22 @@ document.getElementById('book-form').addEventListener('submit', function(e){
   // Instantiate UI
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
+  // Validate
+  if(title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  }else{
+    // Add book to list
+    ui.addBookToList(book);
 
-  // Clear fields
-  ui.clearFields();
+    // Show success
+    ui.showAlert('Book Added', 'success');
+
+    // Clear fields
+    ui.clearFields();
+  }
+
+  
 
   e.preventDefault();
 });
